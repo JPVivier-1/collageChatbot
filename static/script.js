b8bd032b7b637b30b3ec3d2d372c148a548acaac
Jean-Pierre <366562@myaie.ac>
2025-06-15 10:37:36 +0200

    change fetch url

--- static/script.js
+++ static/script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
         addMessage(userText, 'user');
         input.value = '';
 
-        fetch("http://127.0.0.1:5000/chat", {
+        fetch("https://collagechatbot.onrender.com/chat", {
             method: "POST",
             headers: { 
                 "Content-Type" : "application/x-www-form-urlencoded"
